cba23bf5dd085445105bb1f6b01c717fb8d4a7ef
guowenfeng-p <2357647505@qq.com>
2020-06-06 23:26:27 +0800

    第三次提交

---
+++ 增量式开发.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-# 
+# FileName:增量式开发.py 
+# Author:guowenfeng
+# Date:2020/6/6
+# Description: 举个例子，假设你想计算两个给定坐标点 (x1,y1) 和 (x2,y2) 之间的距离
+#---------------------------------------------------------------------
+
+
+def main():
+    """
+        主函数
+    """
+    pass
+
+def distance(x1, y1, x2, y2):
+    return 0.0
+
+
+if __name__ == '__main__':
+    print(distance(1, 2, 4, 6))
